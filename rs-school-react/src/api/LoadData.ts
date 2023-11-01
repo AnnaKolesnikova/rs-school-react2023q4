14edf94c5d091272d427a0c48fa64ead05f5954f
anna <ani.kolesnikova@gmail.com>
2023-11-01 17:02:31 +0100

    feat: add components, data loading, searchTerm

---
+++ rs-school-react/src/api/LoadData.ts
@@ -0,0 +1,29 @@
+import { IPerson } from '../types/types';
+
+class LoadData {
+  API_URL = 'https://swapi.dev/api/people/';
+  getData(searchTerm = ''): Promise<IPerson[]> {
+    return searchTerm ? this.getDataBySearch(searchTerm) : this.getAllData();
+  }
+
+  private getDataBySearch(searchTerm: string): Promise<IPerson[]> {
+    return fetch(`${this.API_URL}/?name=${searchTerm}`)
+      .then((response) => (response.status === 200 ? response.json() : null))
+      .then((data) => (data?.results ? data.results : []));
+  }
+
+  async getAllData(): Promise<IPerson[]> {
+    let allPersons: IPerson[] = [];
+
+    const response = await fetch(this.API_URL);
+    const data = await response.json();
+
+    if (data && data.results) {
+      allPersons = [...allPersons, ...data.results];
+    }
+
+    return allPersons;
+  }
+}
+
+export default LoadData;
