17cd26c7bc043a617f66693f0fe454506baab8c2
anna <ani.kolesnikova@gmail.com>
2023-11-12 22:31:07 +0100

    feat: install testing library, jest, home page context init

--- rs-school-react/src/pages/Home.tsx
+++ rs-school-react/src/pages/Home.tsx
@@ -1,8 +1,26 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 import SearchResults from '../components/SearchResults/SearchResults';
 import SearchSection from '../components/SearchSection/SearchSection';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import { ICharacter } from '../types/types';
+import LoadData from '../api/LoadData';
+
+interface Context {
+  searchWord: string;
+  page: number;
+  updateSearchWord: (val: string) => void;
+  itemData: ICharacter[] | null;
+}
+
+const defaultContext: Context = {
+  searchWord: '',
+  page: 1,
+  updateSearchWord: () => {},
+  itemData: null,
+};
+
+export const HomePageContext = createContext<Context>(defaultContext);
 
 export default function Home() {
   const SEARCH_WORD = 'SearchWord';
@@ -10,12 +28,37 @@ export default function Home() {
   const [searchWord, setSearchWord] = useState(
     storedSearch ? storedSearch : ''
   );
+  const [itemData, setItemData] = useState<ICharacter[] | null>(null);
+  const [loading, setLoading] = useState(false);
   const { page_id } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!page_id || !Number(page_id)) navigate('/page/1', { replace: true });
-  }, [page_id, navigate]);
+
+    const loader = new LoadData();
+    const page = Number(page_id || 1);
+
+    const dataLoading = async (
+      searchWord: string,
+      page: number | undefined
+    ) => {
+      try {
+        const data = await loader.getData(searchWord, page);
+
+        setTimeout(() => {
+          setItemData(data);
+          setLoading(false);
+        }, 250);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
+
+    setLoading(true);
+    dataLoading(searchWord, page);
+  }, [page_id, navigate, searchWord]);
 
   const updateSearchWord = (value: string) => {
     const newValue = value.trim();
@@ -25,14 +68,25 @@ export default function Home() {
       localStorage.setItem(SEARCH_WORD, newValue);
     }
   };
+
+  const context = {
+    searchWord,
+    page: Number(page_id || 1),
+    updateSearchWord,
+    itemData,
+  };
+
   return (
     <ErrorBoundary>
-      <SearchSection
-        searchWord={searchWord}
-        updateSearchWord={updateSearchWord}
-      />
-      <SearchResults searchWord={searchWord} page={+(page_id || 1)} />
-      <Outlet />
+      {loading ? <div className="loading">Loading....</div> : null}
+      <HomePageContext.Provider value={context}>
+        <SearchSection
+          searchWord={searchWord}
+          updateSearchWord={updateSearchWord}
+        />
+        <SearchResults searchWord={searchWord} page={Number(page_id || 1)} />
+        <Outlet />
+      </HomePageContext.Provider>
     </ErrorBoundary>
   );
 }
